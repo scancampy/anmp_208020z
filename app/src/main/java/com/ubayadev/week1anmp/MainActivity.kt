4b5ec6ce0890a4d93847561711cad94e5d0c7d36
Andre <andre@staff.ubaya.ac.id>
2024-08-29 14:36:08 +0700

    nambahin onresume dan ondestroy

---
+++ app/src/main/java/com/ubayadev/week1anmp/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.ubayadev.week1anmp
+
+import android.os.Bundle
+import android.util.Log
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d("state-app", "App is resuming")
+    }
+
+    override fun onDestroy() {
+        Log.d("state-app", "App is closing")
+        super.onDestroy()
+    }
+}
